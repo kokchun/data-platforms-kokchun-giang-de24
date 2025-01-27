0736453da37e6336dd3ea2e586a1c52f82d06d23
kokchun <kokchungiang@gmail.com>
2025-01-27 12:56:19 +0100

    producer

--- code_alongs/08_producer_consumer/src/producer.py
+++ code_alongs/08_producer_consumer/src/producer.py
@@ -11,4 +11,31 @@ with open(data_path / "jokes.json", "r") as file:
     jokes = json.load(file)
 
 
-pprint(jokes)
+# pprint(jokes)
+
+app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
+
+jokes_topic = app.topic(name="jokes", value_serializer="json")
+
+# print(jokes_topic)
+
+
+def main():
+    with app.get_producer() as producer:
+        # print(producer)
+
+        for joke in jokes:
+
+            kafka_msg = jokes_topic.serialize(key=joke["joke_id"], value=joke)
+
+            print(f"Produced message: key = {kafka_msg.key} value = {kafka_msg.value}")
+
+            producer.produce(
+                topic="jokes", key=str(kafka_msg.key), value=kafka_msg.value
+            )
+
+
+# run this code only when executing this script and not when importing this module
+if __name__ == "__main__":
+    # pprint(jokes)
+    main()
