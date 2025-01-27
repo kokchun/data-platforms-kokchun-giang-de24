fcfd10130e5cc38919ee446396a8df564df3213f
kokchun <kokchungiang@gmail.com>
2025-01-27 11:48:10 +0100

    quixstreams

--- code_alongs/08_producer_consumer/src/producer.py
+++ code_alongs/08_producer_consumer/src/producer.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import json
 from pprint import pprint
+from quixstreams import Application
 
 data_path = Path(__file__).parents[1] / "data"
 
-print(data_path)
+# print(data_path)
 
 with open(data_path / "jokes.json", "r") as file:
     jokes = json.load(file)
