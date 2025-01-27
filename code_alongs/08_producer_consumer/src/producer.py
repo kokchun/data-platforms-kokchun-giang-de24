0a01ab5e247b6663b5081ccd2acd9c8882af57e7
kokchun <kokchungiang@gmail.com>
2025-01-27 11:24:00 +0100

    initial setup

---
+++ code_alongs/08_producer_consumer/src/producer.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import json
+from pprint import pprint
+
+data_path = Path(__file__).parents[1] / "data"
+
+print(data_path)
+
+with open(data_path / "jokes.json", "r") as file:
+    jokes = json.load(file)
+
+
+pprint(jokes)
