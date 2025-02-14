20e7313044c3a03460f08c733824eb223736fc8d
kokchun <kokchungiang@gmail.com>
2025-02-14 10:22:16 +0100

    finished code alongs, dashboard working

--- code_alongs/13_live_dashboard/src/charts.py
+++ code_alongs/13_live_dashboard/src/charts.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def line_chart(x, y, **options):
     fig, ax = plt.subplots(1)
 
-    ax.plot(x, y, **options)
+    ax.plot(x, y, linewidth = 3)
 
     ax.set(**options)
 
--- code_alongs/13_live_dashboard/src/dashboard.py
+++ code_alongs/13_live_dashboard/src/dashboard.py
@@ -7,17 +7,22 @@ from constants import (
     POSTGRES_USER,
 )
 from sqlalchemy import create_engine
-import pandas as pd 
+import pandas as pd
+from charts import line_chart
+from streamlit_autorefresh import st_autorefresh
 
 
 connection_string = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DBNAME}"
 
 engine = create_engine(connection_string)
 
+count = st_autorefresh(interval=1000*10, limit=100)
+
 def load_data(query):
-    with engine.connect() as conn: 
+    with engine.connect() as conn:
         df = pd.read_sql(query, conn)
     return df
+
 
 def layout():
     df = load_data("SELECT * FROM bitcoin;")
@@ -33,6 +38,16 @@ def layout():
     st.markdown("## Bitcoin latest price in USD")
 
     # plotting code
+    price_chart = line_chart(
+        df["timestamp"],
+        df["price_usd"],
+        title="price USD for bitcoin",
+        xlabel="time",
+        ylabel="USD",
+    )
+
+    st.pyplot(price_chart)
+
 
 if __name__ == "__main__":
     layout()
